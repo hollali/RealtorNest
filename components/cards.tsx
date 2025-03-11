749dc85d67ff7410882b7a6602768b891671b67e
hollali <dheztinykartel@gmail.com>
2025-03-11 15:13:32 +0000

    feat: enhance FeaturedCard component with additional item details and styling

--- components/cards.tsx
+++ components/cards.tsx
@@ -24,6 +24,23 @@ export const FeaturedCard = ({ item, onPress }: Props) => {
 					{item.rating}
 				</Text>
 			</View>
+			<View className="flex flex-col items-start absolute bottom-5 inset-x-5">
+				<Text
+					className="text-xl font-rubik-extrabold text-white"
+					numberOfLines={1}>
+					{item.name}
+				</Text>
+				<Text className="text-base font-rubik text-white" numberOfLines={1}>
+					{item.address}
+				</Text>
+
+				<View className="flex flex-row items-center justify-between w-full">
+					<Text className="text-xl font-rubik-extrabold text-white">
+						${item.price}
+					</Text>
+					<Image source={icons.heart} className="size-5" />
+				</View>
+			</View>
 		</TouchableOpacity>
 	);
 };
