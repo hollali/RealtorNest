ad8bc545b80393374afdc7d84e637fadc005a83b
hollali <dheztinykartel@gmail.com>
2025-03-11 02:40:20 +0000

    feat: replace View with SafeAreaView in Explore component for improved layout

--- app/(root)/(tabs)/explore.tsx
+++ app/(root)/(tabs)/explore.tsx
@@ -1,11 +1,11 @@
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
 
 const Explore = () => {
 	return (
-		<View>
+		<SafeAreaView>
 			<Text>explore</Text>
-		</View>
+		</SafeAreaView>
 	);
 };
 
