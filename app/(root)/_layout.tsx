0a3eccf42bdd3c31ca9411dd3a66d7945d27560c
hollali <dheztinykartel@gmail.com>
2025-03-13 12:30:48 +0000

    refactor: simplify loading and authentication logic in AppLayout component

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,17 +1,20 @@
 import { useGlobalContext } from "@/lib/global-provider";
 import { Redirect, Slot } from "expo-router";
-import { ActivityIndicator } from "react-native";
+import { ActivityIndicator, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function AppLayout() {
-    const {loading, isLogged} = useGlobalContext();
-    if(loading) {
-        return (
-            <SafeAreaView className="bg-white h-full flex justify-center items-center">
-                <ActivityIndicator className="text-primary-300" size="large" />
-            </SafeAreaView>
-        )
-    }
-    if(!isLogged) return <Redirect href='/sign-in'/>
-    return <Slot/>
+    const { loading, isLogged } = useGlobalContext();
+
+    return loading ? (
+        <SafeAreaView className="bg-white h-full flex justify-center items-center">
+            <View>
+                <ActivityIndicator size="large" color="#3B82F6" />
+            </View>
+        </SafeAreaView>
+    ) : isLogged ? (
+        <Slot />
+    ) : (
+        <Redirect href="/sign-in" />
+    );
 }
