c844ff972367718c364ee87a0d610020aedd8f34
hollali <dheztinykartel@gmail.com>
2025-04-24 07:23:05 +0000

    fix: correct code formatting in Index component for improved readability

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -19,8 +19,7 @@ export default function Index() {
 		<SafeAreaView className="bg-white h-full">
 			<View className="px-5">
 				<View
-					className="flex flex-row items-center 
-				justify-between mt-5">
+					className="flex flex-row items-center justify-between mt-5">
 					<View className="flex flex-row items-center">
 						<Image source={images.avatar} className="size-12 rounded-full" />
 						<View className="flex flex-col items-start ml-2 justify-center">
