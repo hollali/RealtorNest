8dc8390d5260595c1bea81c31edda4e5c1901297
hollali <dheztinykartel@gmail.com>
2025-04-24 06:57:04 +0000

    fix: adjust layout by moving Search component and refining Featured section

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -30,10 +30,15 @@ export default function Index() {
 					</View>
 					<Image source={icons.bell} className="size-6"/>
 				</View>
-			</View>
-			<Search/>
+				<Search/>
 			<View className="my-5">
-				<View></View>
+				<View className="flex flex-row items-center justify-between">
+					<Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
+					<TouchableOpacity>
+						<Text className="text-base font-rubik-bold text-primary-300">See All</Text>
+					</TouchableOpacity>
+				</View>
+			</View>
 			</View>
 		</SafeAreaView>
 	);
