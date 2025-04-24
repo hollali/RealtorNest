b67586efa8b598a4e395edb27cd3c57ba81dc948
hollali <dheztinykartel@gmail.com>
2025-04-24 06:27:05 +0000

    refactor: simplify Index component by removing unused FlatList and FeaturedCard elements

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -17,111 +17,24 @@ import {
 export default function Index() {
 	return (
 		<SafeAreaView className="bg-white h-full">
-			<FlatList 
-				data={[1,2]} 
-				renderItem={({item}) => (
-					<Card
-						item={{
-							$id: item.toString(),
-							$collectionId: "",
-							$databaseId: "",
-							$createdAt: "",
-							$updatedAt: "",
-							$permissions: [],
-						}}
-					/>
-				)}
-				keyExtractor={(item) => item.toString()}
-				numColumns={2}
-				contentContainerClassName="pb-32"
-				columnWrapperClassName="flex gap-5 px-5"
-				ListHeaderComponent={
-					<View className="px-5">
+			<View className="px-5">
 				<View className="flex flex-row items-center justify-between mt-5">
 					<View className="flex flex-row items-center">
-						<Image source={images.avatar} className="size-12 rounded-full" />
+						<Image source={images.avatar} className="size-12 rounded-full"/>
 						<View className="flex flex-col items-start ml-2 justify-center">
-							<Text className="text-xs font-rubik text-black-100">
-								Hi There,
-							</Text>
+							<Text className="text-xs font-rubik text-black-100">Hi,</Text>
 							<Text className="text-base font-rubik-medium text-black-300">
-								Hollali Kelvin
-							</Text>
+								John Doe
+							</Text>	
 						</View>
 					</View>
-					<Image source={icons.bell} className="size-6" />
-				</View>
-				<Search />
-				<View className="my-5">
-					<View className="flex flex-row items-center justify-between">
-						<Text className="text-xl font-rubik-bold text-black-300">
-							Featured
-						</Text>
-						<TouchableOpacity>
-							<Text className="text-base font-rubik-bold text-primary-300">
-								See All
-							</Text>
-						</TouchableOpacity>
-					</View>
-					<View className="flex flex-row gap-5 mt-5">
-						<FeaturedCard
-							item={{
-								$id: "",
-								$collectionId: "",
-								$databaseId: "",
-								$createdAt: "",
-								$updatedAt: "",
-								$permissions: [],
-							}}
-						/>
-						<FeaturedCard
-							item={{
-								$id: "",
-								$collectionId: "",
-								$databaseId: "",
-								$createdAt: "",
-								$updatedAt: "",
-								$permissions: [],
-							}}
-						/>
-					</View>
-				</View>
-				<View className="flex flex-row items-center justify-between">
-					<Text className="text-xl font-rubik-bold text-black-300">
-						Our Recommendation
-					</Text>
-					<TouchableOpacity>
-						<Text className="text-base font-rubik-bold text-primary-300">
-							See All
-						</Text>
-					</TouchableOpacity>
-				</View>
-				<Filters/>
-				<View className="flex flex-row gap-5 mt-5">
-					<Card
-						item={{
-							$id: "",
-							$collectionId: "",
-							$databaseId: "",
-							$createdAt: "",
-							$updatedAt: "",
-							$permissions: [],
-						}}
-					/>
-					<Card
-						item={{
-							$id: "",
-							$collectionId: "",
-							$databaseId: "",
-							$createdAt: "",
-							$updatedAt: "",
-							$permissions: [],
-						}}
-					/>
+					<Image source={icons.bell} className="size-6"/>
 				</View>
 			</View>
-				}
-			/>
+			<Search/>
+			<View className="my-5">
+				<View></View>
+			</View>
 		</SafeAreaView>
 	);
 }
