5611295d0fede0c80bcd8bd1f29708e0eaf9b87d
hollali <dheztinykartel@gmail.com>
2025-04-24 07:17:00 +0000

    fix: improve code formatting for better readability in Index component

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -18,27 +18,33 @@ export default function Index() {
 	return (
 		<SafeAreaView className="bg-white h-full">
 			<View className="px-5">
-				<View className="flex flex-row items-center justify-between mt-5">
+				<View
+					className="flex flex-row items-center 
+				justify-between mt-5">
 					<View className="flex flex-row items-center">
-						<Image source={images.avatar} className="size-12 rounded-full"/>
+						<Image source={images.avatar} className="size-12 rounded-full" />
 						<View className="flex flex-col items-start ml-2 justify-center">
 							<Text className="text-xs font-rubik text-black-100">Hi,</Text>
 							<Text className="text-base font-rubik-medium text-black-300">
 								John Doe
-							</Text>	
+							</Text>
 						</View>
 					</View>
-					<Image source={icons.bell} className="size-6"/>
+					<Image source={icons.bell} className="size-6" />
 				</View>
-				<Search/>
-			<View className="my-5">
-				<View className="flex flex-row items-center justify-between">
-					<Text className="text-xl font-rubik-bold text-black-300">Featured</Text>
-					<TouchableOpacity>
-						<Text className="text-base font-rubik-bold text-primary-300">See All</Text>
-					</TouchableOpacity>
+				<Search />
+				<View className="my-5">
+					<View className="flex flex-row items-center justify-between">
+						<Text className="text-xl font-rubik-bold text-black-300">
+							Featured
+						</Text>
+						<TouchableOpacity>
+							<Text className="text-base font-rubik-bold text-primary-300">
+								See All
+							</Text>
+						</TouchableOpacity>
+					</View>
 				</View>
-			</View>
 			</View>
 		</SafeAreaView>
 	);
