392f8a418a3feeb78a9f72d0a3b4e08456fdfa11
hollali <dheztinykartel@gmail.com>
2025-04-24 19:44:49 +0000

    refactor: simplify component structure and improve readability in Index and Card components

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -18,8 +18,7 @@ export default function Index() {
 	return (
 		<SafeAreaView className="bg-white h-full">
 			<View className="px-5">
-				<View
-					className="flex flex-row items-center justify-between mt-5">
+				<View className="flex flex-row items-center justify-between mt-5">
 					<View className="flex flex-row items-center">
 						<Image source={images.avatar} className="size-12 rounded-full" />
 						<View className="flex flex-col items-start ml-2 justify-center">
@@ -45,6 +44,8 @@ export default function Index() {
 					</View>
 				</View>
 			</View>
+			<FeaturedCard />
+			<Card/>
 		</SafeAreaView>
 	);
 }
