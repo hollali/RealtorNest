73ff8d9cbdc0e676f40335739d243a2946c094dd
hollali <dheztinykartel@gmail.com>
2025-04-26 19:44:08 +0000

    `Remove accessibility attributes from Search input component`

--- components/search.tsx
+++ components/search.tsx
@@ -29,9 +29,7 @@ const Search = () => {
 					onChangeText={handleSearch}
 					placeholder="Search"
 					placeholderTextColor="#A0A0A0"
-					className="text-sm font-rubik text-black-300 ml-2 flex-1 border-r-4"
-					accessibilityLabel="Search input"
-					accessible={true}
+					className="text-sm font-rubik text-black-300 ml-2 flex-1"
 				/>
 			</View>
 			<TouchableOpacity>
