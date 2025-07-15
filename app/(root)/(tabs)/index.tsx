e3c9288fd32207d92370d02603cde18cd2af9307
hollali <dheztinykartel@gmail.com>
2025-07-15 15:20:56 +0000

    `Refactored Home component in tabs/index.tsx, removing 26 lines of code`

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -9,23 +9,18 @@ import {
 import { useEffect,useMemo } from "react";
 import { router, useLocalSearchParams } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
-
 import icons from "@/constants/icons";
-
 import Search from "@/components/search";
 import Filters from "@/components/filters";
 import NoResults from "@/components/noResults";
 import { Card, FeaturedCard } from "@/components/cards";
-
 import { useAppwrite } from "@/lib/useAppwrite";
 import { useGlobalContext } from "@/lib/global-provider";
 import { getLatestProperties, getProperties } from "@/lib/appwrite";
 
 const Home = () => {
 	const { user } = useGlobalContext();
-
 	const params = useLocalSearchParams<{ query?: string; filter?: string }>();
-
 	const { data: latestProperties, loading: latestPropertiesLoading } =
 		useAppwrite({
 			fn: getLatestProperties,
@@ -103,9 +98,7 @@ const greeting = useMemo(() => {
 							</View>
 							<Image source={icons.bell} className="size-6" />
 						</View>
-
 						<Search />
-
 						<View className="my-5">
 							<View className="flex flex-row items-center justify-between">
 								<Text className="text-xl font-rubik-bold text-black-300">
@@ -138,9 +131,7 @@ const greeting = useMemo(() => {
 								/>
 							)}
 						</View>
-
 						{/* <Button title="seed" onPress={seed} /> */}
-
 						<View className="mt-5">
 							<View className="flex flex-row items-center justify-between">
 								<Text className="text-xl font-rubik-bold text-black-300">
@@ -152,7 +143,6 @@ const greeting = useMemo(() => {
 									</Text>
 								</TouchableOpacity>
 							</View>
-
 							<Filters />
 						</View>
 					</View>
