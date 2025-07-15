d2933f0c32800a42d5e1f6afe76791a366fb66b7
hollali <dheztinykartel@gmail.com>
2025-07-15 16:35:53 +0000

    `Removed 24 lines of code from Property component`

--- app/(root)/properties/[id].tsx
+++ app/(root)/properties/[id].tsx
@@ -9,20 +9,16 @@ import {
 	Platform,
 } from "react-native";
 import { router, useLocalSearchParams } from "expo-router";
-
 import icons from "@/constants/icons";
 import images from "@/constants/images";
 import Comment from "@/components/comment";
 import { facilities } from "@/constants/data";
-
 import { useAppwrite } from "@/lib/useAppwrite";
 import { getPropertyById } from "@/lib/appwrite";
 
 const Property = () => {
 	const { id } = useLocalSearchParams<{ id?: string }>();
-
 	const windowHeight = Dimensions.get("window").height;
-
 	const { data: property } = useAppwrite({
 		fn: getPropertyById,
 		params: {
@@ -45,7 +41,6 @@ const Property = () => {
 						source={images.whiteGradient}
 						className="absolute top-0 w-full z-40"
 					/>
-
 					<View
 						className="z-50 absolute inset-x-7"
 						style={{
@@ -57,7 +52,6 @@ const Property = () => {
 								className="flex flex-row bg-primary-200 rounded-full size-11 items-center justify-center">
 								<Image source={icons.backArrow} className="size-5" />
 							</TouchableOpacity>
-
 							<View className="flex flex-row items-center gap-3">
 								<Image
 									source={icons.heart}
@@ -69,19 +63,16 @@ const Property = () => {
 						</View>
 					</View>
 				</View>
-
 				<View className="px-5 mt-7 flex gap-2">
 					<Text className="text-2xl font-rubik-extrabold">
 						{property?.name}
 					</Text>
-
 					<View className="flex flex-row items-center gap-3">
 						<View className="flex flex-row items-center px-4 py-2 bg-primary-100 rounded-full">
 							<Text className="text-xs font-rubik-bold text-primary-300">
 								{property?.type}
 							</Text>
 						</View>
-
 						<View className="flex flex-row items-center gap-2">
 							<Image source={icons.star} className="size-5" />
 							<Text className="text-black-200 text-sm mt-1 font-rubik-medium">
@@ -89,7 +80,6 @@ const Property = () => {
 							</Text>
 						</View>
 					</View>
-
 					<View className="flex flex-row items-center mt-5">
 						<View className="flex flex-row items-center justify-center bg-primary-100 rounded-full size-10">
 							<Image source={icons.bed} className="size-4" />
@@ -110,19 +100,16 @@ const Property = () => {
 							{property?.area} sqft
 						</Text>
 					</View>
-
 					<View className="w-full border-t border-primary-200 pt-7 mt-5">
 						<Text className="text-black-300 text-xl font-rubik-bold">
 							Agent
 						</Text>
-
 						<View className="flex flex-row items-center justify-between mt-4">
 							<View className="flex flex-row items-center">
 								<Image
 									source={{ uri: property?.agent.avatar }}
 									className="size-14 rounded-full"
 								/>
-
 								<View className="flex flex-col items-start justify-center ml-3">
 									<Text className="text-lg text-black-300 text-start font-rubik-bold">
 										{property?.agent.name}
@@ -132,14 +119,12 @@ const Property = () => {
 									</Text>
 								</View>
 							</View>
-
 							<View className="flex flex-row items-center gap-3">
 								<Image source={icons.chat} className="size-7" />
 								<Image source={icons.phone} className="size-7" />
 							</View>
 						</View>
 					</View>
-
 					<View className="mt-7">
 						<Text className="text-black-300 text-xl font-rubik-bold">
 							Overview
@@ -148,19 +133,16 @@ const Property = () => {
 							{property?.description}
 						</Text>
 					</View>
-
 					<View className="mt-7">
 						<Text className="text-black-300 text-xl font-rubik-bold">
 							Facilities
 						</Text>
-
 						{property?.facilities.length > 0 && (
 							<View className="flex flex-row flex-wrap items-start justify-start mt-2 gap-5">
 								{property?.facilities.map((item: string, index: number) => {
 									const facility = facilities.find(
 										(facility) => facility.title === item
 									);
-
 									return (
 										<View
 											key={index}
@@ -171,7 +153,6 @@ const Property = () => {
 													className="size-6"
 												/>
 											</View>
-
 											<Text
 												numberOfLines={1}
 												ellipsizeMode="tail"
@@ -184,7 +165,6 @@ const Property = () => {
 							</View>
 						)}
 					</View>
-
 					{property?.gallery.length > 0 && (
 						<View className="mt-7">
 							<Text className="text-black-300 text-xl font-rubik-bold">
@@ -206,7 +186,6 @@ const Property = () => {
 							/>
 						</View>
 					)}
-
 					<View className="mt-7">
 						<Text className="text-black-300 text-xl font-rubik-bold">
 							Location
@@ -217,13 +196,11 @@ const Property = () => {
 								{property?.address}
 							</Text>
 						</View>
-
 						<Image
 							source={images.map}
 							className="h-52 w-full mt-5 rounded-xl"
 						/>
 					</View>
-
 					{property?.reviews.length > 0 && (
 						<View className="mt-7">
 							<View className="flex flex-row items-center justify-between">
@@ -233,14 +210,12 @@ const Property = () => {
 										{property?.rating} ({property?.reviews.length} reviews)
 									</Text>
 								</View>
-
 								<TouchableOpacity>
 									<Text className="text-primary-300 text-base font-rubik-bold">
 										View All
 									</Text>
 								</TouchableOpacity>
 							</View>
-
 							<View className="mt-5">
 								<Comment item={property?.reviews[0]} />
 							</View>
@@ -248,7 +223,6 @@ const Property = () => {
 					)}
 				</View>
 			</ScrollView>
-
 			<View className="absolute bg-white bottom-0 w-full rounded-t-2xl border-t border-r border-l border-primary-200 p-7">
 				<View className="flex flex-row items-center justify-between gap-10">
 					<View className="flex flex-col items-start">
@@ -261,7 +235,6 @@ const Property = () => {
 							${property?.price}
 						</Text>
 					</View>
-
 					<TouchableOpacity className="flex-1 flex flex-row items-center justify-center bg-primary-300 py-3 rounded-full shadow-md shadow-zinc-400">
 						<Text className="text-white text-lg text-center font-rubik-bold">
 							Book Now
