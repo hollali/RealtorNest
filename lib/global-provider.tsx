a9acc59520df36ae1941fb39f9a0e2c4f55ee997
hollali <dheztinykartel@gmail.com>
2025-07-17 08:15:23 +0000

    `Removed unnecessary line from useGlobalContext function`

--- lib/global-provider.tsx
+++ lib/global-provider.tsx
@@ -51,7 +51,6 @@ export const useGlobalContext = (): GlobalContextType => {
 	const context = useContext(GlobalContext);
 	if (!context)
 		throw new Error("useGlobalContext must be used within a GlobalProvider");
-
 	return context;
 };
 
